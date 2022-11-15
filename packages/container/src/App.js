d58a9643b92f5cc5d7d89425858c4eb961faf991
Stefan Nestorovic <stefpepp@gmail.com>
2022-11-15 17:43:22 +0100

    see if action starts

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,8 +2,10 @@ import React from 'react';
 import MarketingApp from './components/MarketingApp';
 
 export default ()=>{
-    return <div>
-        <h1>Container</h1>
-        <MarketingApp/>
-        </div>
+    return (
+      <div>
+        <h1>Container header</h1>
+        <MarketingApp />
+      </div>
+    );
 }
