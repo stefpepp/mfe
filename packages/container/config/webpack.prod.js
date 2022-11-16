5b60a74d6aa6b3cbcc3ae55cdcd7c0c3f9f6d74f
Stefan Nestorovic <stefpepp@gmail.com>
2022-11-16 13:13:42 +0100

    fix typo

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -9,7 +9,7 @@ const prodConfig = {
   mode: "production",
   output: {
     filename: "[name].[contenthash].js",
-    publcPath: "/container/latest/",
+    publicPath: "/container/latest/",
   },
   plugins: [
     new ModuleFederationPlugin({
