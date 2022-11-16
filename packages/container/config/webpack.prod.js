962e0d58dc1bea8d8d0264f78ca2bc85ad2fe144
Stefan Nestorovic <stefpepp@gmail.com>
2022-11-16 13:11:27 +0100

    added public path for webpack build files, on aws s3

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -9,6 +9,7 @@ const prodConfig = {
   mode: "production",
   output: {
     filename: "[name].[contenthash].js",
+    publcPath: "/container/latest/",
   },
   plugins: [
     new ModuleFederationPlugin({
